204b52883780ad989e0c690fa1f7747f77b93ebd
Tyler Schmidt <schmidt197@gmail.com>
2016-10-31 18:32:29 -0700

    this is my first commit. all hail pylon

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,20 @@
+var axios =require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=982013f95d0f982b6fe12bde4258efce&units=imperial';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
