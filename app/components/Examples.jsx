98a811a1339eb5be6c952147c088107f0e2c164b
Tyler Schmidt <schmidt197@gmail.com>
2016-11-02 10:21:36 -0700

    added grid styles to my main and examples divs.

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 
 var Examples = (props) => {
   return(
     <div>
-      <h3>Examples!</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples!</h1>
+      <p>Here are a few example location to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Richmond'>Richmond, VA</Link>
+        </li>
+        <li>
+          <Link to='/?location=San Francisco'>San Francisco, CA</Link>
+        </li>
+      </ol>
     </div>
   )
 };
