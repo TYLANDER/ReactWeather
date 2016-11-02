2b230b83af96e7825cd4b3bbcb6e8322554cee08
Tyler Schmidt <schmidt197@gmail.com>
2016-11-02 15:05:00 -0700

    added css styling to my app

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var About = (props) => {
   return(
     <div>
       <div>
-        <h1 className="text-center">About</h1>
+        <h1 className="text-center page-title">About</h1>
         <p>
           This is a weather application built on React. I have built this for complete react web app dev course.
         </p>
